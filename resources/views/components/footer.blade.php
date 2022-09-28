15756f5c80f31d280efcfb2c0fe335b462b32f69
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-09-28 20:30:46 +0700

    sidebar dan footer selesai, juga perbaikan navbar

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+@include('components.head')
+<footer class="p-4 bg-white shadow-lg md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span class="text-lg text-gray-500 sm:text-center dark:text-gray-400">Copyright © 2022 <a class="hover:underline font-extrabold text-mainclr">PLN</a>. All Rights Reserved.
+    </span>
+    <div class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <p class="text-lg font-extrabold text-black">ETM
+            <span class="font-normal">
+                System
+        </span>  
+        </p>
+    </div>
+</footer>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,18 @@
 @include('components.head')
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-    <div class="container flex flex-wrap justify-between items-center mx-auto shadow-lg py-4">
-      <div class="flex items-center ml-32">
+<nav class="bg-white border-gray-200">
+    <div class="p-4 bg-white shadow-lg md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <div class="flex items-center">
             <p class="text-black font-extrabold text-2xl">
                 Hello, Admin
             </p>
+            <button data-collapse-toggle="navbar-default" type="button" class="absolute right-4 top-3 p-2 ml-3 text-sm" aria-controls="navbar-default" aria-expanded="false">
+                <img src="imgs/lingkaran.png" alt="" class="absolute w-4">
+                <p class="absolute text-white ml-1 text-xs">4</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-8 items-center"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg>
+            </button>
+              <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                
+              </div>
       </div>
     </div>
-    <button data-collapse-toggle="navbar-default" type="button" class="absolute right-4 top-3 p-2 ml-3 text-sm" aria-controls="navbar-default" aria-expanded="false">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-7 items-center"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg>
-      </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        
-      </div>
   </nav>
