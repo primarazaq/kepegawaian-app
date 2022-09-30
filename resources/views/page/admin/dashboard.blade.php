638033147d572c4ae46189e35fbc0f04bcce59f5
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-09-30 13:35:04 +0700

    sementara

--- resources/views/page/admin/dashboard.blade.php
+++ resources/views/page/admin/dashboard.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
     
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
       Dashboard
-      </div>
-    </div><hr>
-  </nav>
   
 @endsection
