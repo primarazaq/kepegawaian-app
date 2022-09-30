b161687254dc40ad5d978cbfead88e0e26afa16a
primarazaq <85297003+primarazaq@users.noreply.github.com>
2022-10-01 02:11:27 +0700

    creating eloquent relationship many to many

---
+++ database/migrations/2022_09_29_182737_create_user_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_sender_id');
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('user_receiver_id');
+            // $table->unsignedBigInteger('user_receiver2_id')->nullable();
+            // $table->unsignedBigInteger('user_receiver3_id')->nullable();
+            //foreignkey
+            $table->foreign('user_sender_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_receiver_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreign('user_receiver2_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreign('user_receiver3_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tasks');
+    }
+};
