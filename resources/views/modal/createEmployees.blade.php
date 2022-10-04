9d838f6cff445249ab6fe533d636120c97a6bb30
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-10-04 13:00:52 +0700

    revisi tampilan

--- resources/views/modal/createEmployees.blade.php
+++ resources/views/modal/createEmployees.blade.php
@@ -54,18 +54,18 @@
                     </div>
                     <div class="relative">
                         <label for="tanggal-mulai" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Tanggal Mulai</label>
-                        <input datepicker datepicker-autohide type="date" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-mainclr focus:border-mainclr block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" placeholder="Select date">
+                        <input type="text" id="disabled-input-2" aria-label="disabled input-2" class="mb-2 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainclr focus:border-mainclr block w-full p-2 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-mainclr dark:focus:border-mainclr" value="21/06/2020" disabled>
                     </div>
                     <div>
                         <label for="status" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Status</label>
                         <div class="flex">
                             <div class="flex w-full items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-                                <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-mainclr bg-gray-100 border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="bordered-radio-1" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">PIC</label>
+                                <input id="bordered-radio-createEMP1" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-mainclr bg-gray-100 border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="bordered-radio-createEMP1" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">PIC</label>
                             </div>
                             <div class="flex w-full items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-                                <input checked id="bordered-radio-2" type="radio" value="" name="bordered-radio" class="w-4 h-4 bg-gray-100 text-mainclr border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="bordered-radio-2" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">Pegawai</label>
+                                <input checked id="bordered-radio-createEMP2" type="radio" value="" name="bordered-radio" class="w-4 h-4 bg-gray-100 text-mainclr border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="bordered-radio-createEMP2" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">Pegawai</label>
                             </div>
                         </div>
                     </div>
