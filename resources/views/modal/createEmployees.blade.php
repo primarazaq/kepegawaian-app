6522dabef3c0c12761b821aab8954858c2568c0c
primarazaq <85297003+primarazaq@users.noreply.github.com>
2022-10-05 12:22:03 +0700

    create validation message in modal form

--- resources/views/modal/createEmployees.blade.php
+++ resources/views/modal/createEmployees.blade.php
@@ -6,7 +6,7 @@
 </button>
 
 <!-- Main modal -->
-  <div id="ModalCreateEMP" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
+  <div id="ModalCreateEMP" @if (count($errors) > 0) data-modal-show="true" @endif tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
       <div class="relative p-4 w-full max-w-2xl h-full md:h-auto ">
           <!-- Modal content -->
           <div class="relative bg-white rounded-xl border-mainclr border-4 shadow dark:bg-gray-700">
@@ -44,33 +44,43 @@
                 <div class="p-5">
                     <div>
                         <label for="name" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Nama Pegawai</label>
-                        <input type="text" id="name" name="name" class="mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr">
+                        <input type="text" id="name" name="name" class="@error('name') invalid:border-red-500 @enderror mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr " autofocus required value="{{ old('name') }}">
+                        @error('name')
+                            <div class="text-red-500 text-xs font-medium">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="relative">
                         <label for="tgl_lhr" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Tanggal Lahir</label>
-                        <input datepicker datepicker-autohide type="date" id="tgl_lhr" name="tgl_lhr" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-mainclr focus:border-mainclr block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" placeholder="Select date">
+                        <input datepicker datepicker-autohide type="date" id="tgl_lhr" name="tgl_lhr" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-mainclr focus:border-mainclr block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" placeholder="Select date" required value="{{ old('tgl_lhr') }}">
                     </div>
                     <div>
                         <label for="nip" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">NIP</label>
-                        <input type="text" id="nip" name="nip" class="mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr">
+                        <input type="text" id="nip" name="nip" class="@error('nip') invalid:border-red-500 @enderror mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" required value="{{ old('nip') }}">
+                        @error('nip')
+                            <div class="text-red-500 text-xs font-medium">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div>
                         <label for="password" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
-                        <input type="password" id="password" name="password" class="mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr">
+                        <input type="password" id="password" name="password" class="mb-2 block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-mainclr focus:border-mainclr dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" required value="{{ old('password') }}">
                     </div>
-                    <div class="hidden relative">
+                    {{-- <div class="hidden relative">
                         <label for="created_at" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Tanggal Mulai</label>
                         <input  type="text" id="created_at" name="created_at" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-mainclr focus:border-mainclr block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainclr dark:focus:border-mainclr" value="<?php echo date("H:i:s - d F Y") ?>" placeholder="<?php echo date("H:i:s - d F Y") ?>" disabled> 
-                    </div>
+                    </div> --}}
                     <div>
                         <label for="level" class="block w-fit text-sm font-medium text-gray-900 dark:text-gray-300">Level</label>
                         <div class="flex">
                             <div class="flex w-full items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-                                <input id="level" name="level" type="radio" value="pic" name="bordered-radio" class="w-4 h-4 text-mainclr bg-gray-100 border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <input id="level" name="level" type="radio" value="pic" class="w-4 h-4 text-mainclr bg-gray-100 border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required value="{{ old('level') }}">
                                 <label for="level" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">PIC</label>
                             </div>
                             <div class="flex w-full items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-                                <input id="level" name="level" type="radio" value="employee" name="bordered-radio" class="w-4 h-4 bg-gray-100 text-mainclr border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <input id="level" name="level" type="radio" value="employee" class="w-4 h-4 bg-gray-100 text-mainclr border-gray-300 focus:ring-mainclr dark:focus:ring-mainclr dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required value="{{ old('level') }}">
                                 <label for="level" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">Pegawai</label>
                             </div>
                         </div>
@@ -78,10 +88,16 @@
                 </div>
               <!-- Modal footer -->
               <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200  dark:border-gray-600">
-                  <button data-modal-toggle="ModalCreateEMP" type="submit" class="text-white mx-auto bg-mainclr font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-teal-500 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800">+ Create</button>
+                  <button type="submit" class="text-white mx-auto bg-mainclr font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-teal-500 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800">+ Create</button>
               </div>
+              
             </form>
           </div>
       </div>
   </div>
+
+
+  @if (count($errors) > 0)
+  data-modal-show="true"
+@endif
   
