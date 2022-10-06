81e0564e7d95e72b09413e5cbc781e14ed0ffbba
tyohd <andr9422@gmail.com>
2022-10-06 21:33:12 +0700

    add error condition

--- resources/views/modal/createEmployees.blade.php
+++ resources/views/modal/createEmployees.blade.php
@@ -6,7 +6,7 @@
 </button>
 
 <!-- Main modal -->
-  <div id="ModalCreateEMP" @if (count($errors) > 0) data-modal-show="true" @endif tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
+  <div id="ModalCreateEMP"  tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
       <div class="relative p-4 w-full max-w-2xl h-full md:h-auto ">
           <!-- Modal content -->
           <div class="relative bg-white rounded-xl border-mainclr border-4 shadow dark:bg-gray-700">
