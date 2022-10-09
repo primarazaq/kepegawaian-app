f833f20b7bf05fb28a1a08f115a9c9d68e89c10a
primarazaq <primarazaq34@gmail.com>
2022-10-09 15:48:31 +0700

    add error count

--- resources/views/modal/createEmployees.blade.php
+++ resources/views/modal/createEmployees.blade.php
@@ -5,8 +5,30 @@
     </p>
 </button>
 
+{{-- <script>
+    let true = "true";
+    document.addEventListener("DOMContentLoaded", function(){
+        let createmodal = document.getElementById('ModalCreateEMP');
+        createmodal.addEventListener('click', function () {
+        if ({{ count($errors) >= 1 }}){
+               createmodal.parentElement.data-modal-show.add('true');
+            }
+
+        }});
+</script> --}}
+
+{{-- // <script type="text/javascript">
+// let createmodal = document.getElementById('ModalCreateEMP');
+//     // $(createmodal).on('load', function(){
+//     //     createmodal.data-modal-show = "true";
+//     // });
+//     createmodal.addEventListener('click', function () {
+//         createmodal.parentElement.data-modal-show.add('true');
+//                             })
+// </script> --}}
+
 <!-- Main modal -->
-  <div id="ModalCreateEMP"  tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
+  <div id="ModalCreateEMP"  @if (count($errors) > 0) data-modal-show="true" @endif tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center ">
       <div class="relative p-4 w-full max-w-2xl h-full md:h-auto ">
           <!-- Modal content -->
           <div class="relative bg-white rounded-xl border-mainclr border-4 shadow dark:bg-gray-700">
