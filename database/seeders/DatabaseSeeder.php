9796b1659d66f499963d2a1a3c409ee5418994f8
tyohd <andr9422@gmail.com>
2022-10-12 21:40:07 +0700

    fix due date

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -61,7 +61,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong ikhsan buat dokumen penyalaan listrik',
             't_status' => 'in progress',
             't_priority' => 'high',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-10-14',
         ]);
 
         Task::create([
@@ -69,7 +69,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong test buat dokumen penyalaan listrik',
             't_status' => 'uncompleted',
             't_priority' => 'medium',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-21',
         ]);
 
         Task::create([
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong hilman buat dokumen A',
             't_status' => 'in progress',
             't_priority' => 'high',
-            't_due_date' => '2022-10-02',
+            't_due_date' => '2022-12-22',
         ]);
 
         Task::create([
@@ -85,7 +85,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin C',
             't_status' => 'completed',
             't_priority' => 'medium',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-23',
         ]);
 
         Task::create([
@@ -93,7 +93,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin D',
             't_status' => 'in progress',
             't_priority' => 'high',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-24',
         ]);
 
         Task::create([
@@ -101,7 +101,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin E',
             't_status' => 'completed',
             't_priority' => 'medium',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-25',
         ]);
 
         Task::create([
@@ -109,7 +109,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin F',
             't_status' => 'in progress',
             't_priority' => 'high',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-26',
         ]);
 
         Task::create([
@@ -117,7 +117,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin G',
             't_status' => 'in progress',
             't_priority' => 'medium',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-27',
         ]);
 
         Task::create([
@@ -125,7 +125,7 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin H',
             't_status' => 'in progress',
             't_priority' => 'high',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-28',
         ]);
 
         Task::create([
@@ -133,64 +133,64 @@ class DatabaseSeeder extends Seeder
             't_body' => 'tolong buatin I',
             't_status' => 'in progress',
             't_priority' => 'medium',
-            't_due_date' => '2022-10-01',
+            't_due_date' => '2022-12-29',
         ]);
 
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '1',
             'user_receiver_id' => '3',
         ]);
 
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '2',
             'user_receiver_id' => '3',
         ]);
 
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '2',
             'user_receiver_id' => '3',
         ]);
 
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '3',
             'user_receiver_id' => '4',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '4',
             'user_receiver_id' => '3',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '5',
             'user_receiver_id' => '4',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '6',
             'user_receiver_id' => '3',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '7',
             'user_receiver_id' => '4',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '8',
             'user_receiver_id' => '3',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '9',
             'user_receiver_id' => '4',
         ]);
         UserTask::create([
-            'user_sender_id' => '1',
+            'user_sender_id' => '2',
             'task_id' => '10',
             'user_receiver_id' => '3',
         ]);
