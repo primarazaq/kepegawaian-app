b0ee507db113d99419096333773b62c583a28f1a
tyohd <andr9422@gmail.com>
2022-10-12 21:40:17 +0700

    logika date

--- resources/views/page/employee/mytask.blade.php
+++ resources/views/page/employee/mytask.blade.php
@@ -34,24 +34,51 @@
 </div>
 <div class="mx-auto w-11/12 border-2 shadow-md border-t-mainclr rounded-2xl">
     {{-- flex 1 --}}
+    @foreach ($taskList as $data)
+    <?php 
+    //calculate time left
+        $seconds = strtotime($data->t_due_date) - time();
+
+        $days = floor($seconds / 86400);
+        $seconds %= 86400;
+
+        $hours = floor($seconds / 3600);
+        $seconds %= 3600;
+
+        $minutes = floor($seconds / 60);
+        $seconds %= 60;
+
+        $date = date('d-m-Y H-i',strtotime($data->t_due_date));
+    ?>
     <div class="flex border-b-2 border-black">
         <div class="basis-4/5">
-            <p class="font-extrabold text-2xl ml-4 mt-3">Task 1</p>
-            <p class="font-medium text-md ml-8">Memperbaiki dokumen penyalaan pada pelanggan A</p>
+            <p class="font-extrabold text-2xl ml-4 mt-3">Task {{ $loop->iteration }}</p>
+            <p class="font-medium text-sm text-red-500 ml-8">Deadline :<?php echo "$date WIB" ?></p>
+            <p class="font-medium text-md ml-8">{{ $data->t_title }}</p>
             <div class="flex mb-8 ">
-                <div class="w-56 bg-gray-300 rounded-full h-2.5 ml-8 mt-2.5 dark:bg-gray-700">
-                    <div class="bg-mainclr h-2.5 rounded-full" style="width: 45%"></div>
+                <div class="w-56 h-2.5 ml-8 mt-1 dark:bg-gray-700">
+                    PIC : {{ $data->name }}
                 </div>
-                <p class="inline-flex ml-1 mt-1">45%</p>
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 ml-5 inline-flex" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 448C48 456.8 55.16 464 64 464H384C392.8 464 400 456.8 400 448V192H48V448z"/></svg>
-                <p class="inline-flex ml-4 text-gray-400 mt-1">Due in 10 days</p>
+                <?php
+                    if ($days <=0) {
+                        ?> <p class="inline-flex ml-4 mt-1"><?php echo "$hours hours and $minutes minutes left" ?></p> <?php
+                    } elseif ($hours <= 0) {
+                        ?> <p class="inline-flex ml-4 mt-1"><?php echo "$minutes minutes left" ?></p> <?php
+                    } else {
+                        ?> <p class="inline-flex ml-4 mt-1"><?php echo "$days days $hours hours and $minutes minutes left" ?></p> <?php
+                    }
+                ?>
+                
+                
             </div>
         </div>
         {{-- flex 2 --}}
         @include('modal.mytask')
     </div>
+    @endforeach
     {{-- flex 1 --}}
-    <div class="flex border-b-2 border-black">
+    {{-- <div class="flex border-b-2 border-black">
         <div class="basis-4/5">
             <p class="font-extrabold text-2xl ml-4 mt-3">Task 2</p>
             <p class="font-medium text-md ml-8">Memperbaiki dokumen penyalaan pada pelanggan A</p>
@@ -64,11 +91,10 @@
                 <p class="inline-flex ml-4 text-gray-400 mt-1">Due in 10 days</p>
             </div>
         </div>
-        {{-- flex 2 --}}
         @include('modal.mytask')
-    </div>
+    </div> --}}
     {{-- flex 1 --}}
-    <div class="flex">
+    {{-- <div class="flex">
         <div class="basis-4/5">
             <p class="font-extrabold text-2xl ml-4 mt-3">Task 3</p>
             <p class="font-medium text-md ml-8">Memperbaiki dokumen penyalaan pada pelanggan A</p>
@@ -81,8 +107,7 @@
                 <p class="inline-flex ml-4 text-gray-400 mt-1">Due in 10 days</p>
             </div>
         </div>
-        {{-- flex 2 --}}
         @include('modal.mytask')
-    </div>
+    </div> --}}
 </div>
 @endsection
