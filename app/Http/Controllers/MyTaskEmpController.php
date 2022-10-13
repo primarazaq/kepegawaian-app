14e864343b6ea69b816a71e19b8a372e4dd147b2
primarazaq <85297003+primarazaq@users.noreply.github.com>
2022-10-14 02:16:08 +0700

    fixing controller for mytask

--- app/Http/Controllers/MyTaskEmpController.php
+++ app/Http/Controllers/MyTaskEmpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\User;
+use App\Models\UserTask;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -26,14 +27,13 @@ class MyTaskEmpController extends Controller
         //             ->orderBy('b.id' , 'asc')
         //             ->get();
         $task = DB::table('users as a')
-                    ->select('b.*', 'a.*', 'aa.*')
-                    ->where('c.user_receiver_id',auth()->user()->id)
+                    ->select('b.t_due_date', 'a.id as receiver_id', 'a.name as receiver_name', 'b.id as t_id', 'b.t_title', 'b.t_file', 'b.t_body', 'b.t_status', 'b.t_priority', 'aa.id as sender_id', 'aa.name as sender_name')
+                    ->where('c.user_receiver_id',auth()->user()->id)->where('b.t_status', 'in progress')
                     ->join('user_tasks as c', 'c.user_receiver_id', '=', 'a.id')
                     ->join('tasks as b', 'b.id', '=', 'c.task_id')
                     ->join('users as aa', 'aa.id', '=', 'c.user_sender_id')
                     ->orderBy('b.id' , 'asc')
                     ->get();
-       
         // $task['t_due_date']->diffForHumans('null, true, true, 2');
 
 
@@ -41,7 +41,7 @@ class MyTaskEmpController extends Controller
         $taskCompleted = Task::where('t_status', 'completed')->get();
         $taskInProgress = Task::where('t_status', 'in progress')->get();
         $taskUncompleted = Task::where('t_status', 'uncompleted')->get();
-
+        
         return view('page.employee.mytask', [
             'taskList' => $task, 
             'userList' => $users,
@@ -103,9 +103,39 @@ class MyTaskEmpController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $task_id)
     {
-        //
+        // $data = $request->validate([
+        //     'response_file' => 'mimes:jpeg,jpg,png,docx,doc,pptx,ppt,xlsx,xls,pdf,zip,rar|file|max:10240',
+        //     'response_body' => 'required'
+        // ]);
+
+        $rules =[
+            'response_file' => 'mimes:jpeg,jpg,png,docx,doc,pptx,ppt,xlsx,xls,pdf,zip,rar|file|max:10240',
+            'response_body' => 'required'
+        ];
+
+        $validatedData = $request->validate($rules);
+
+        $validatedData['response_file'] = $request->file('response_file')->store('task-file');
+
+        // dd($validatedData);
+        $sender_id = $request['user_sender_id'];
+        $receiver_id = auth()->user()->id;
+        // $task = UserTask::where('task_id',$task_id)->get();
+        UserTask::where('task_id',$task_id)
+                ->where('user_sender_id',$sender_id)
+                ->where('user_receiver_id',$receiver_id)
+                ->update($validatedData);
+                // ->update([
+                //     'response_file' => $request['response_file'],
+                //     'response_body' => $request['response_body']
+                // ]);
+        // dd($task);
+        return redirect('/employee/home/mytask')->with('success','Task Berhasil dikirimkan!');
+
+
+        
     }
 
     /**
