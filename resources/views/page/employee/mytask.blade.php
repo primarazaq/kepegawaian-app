940960efd428dc7bb06dbf82a01927026d66229d
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-10-14 20:27:08 +0700

    test

--- resources/views/page/employee/mytask.blade.php
+++ resources/views/page/employee/mytask.blade.php
@@ -56,13 +56,6 @@
             <p class="font-medium text-sm text-red-500 ml-8">Deadline :<?php echo "$date" ?></p>
             <p class="font-medium text-md ml-8">{{ $data->t_title }}</p>
             <div class="flex mb-8 ">
-<<<<<<< HEAD
-                <p class=" ml-8 mt-1">Pengirim</p>
-                <p class="inline-flex mt-1 mr-2">:</p>
-                <p class="mt-1 mr-2">Ikhsan Nurul Rizki</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 ml-5 inline-flex" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 448C48 456.8 55.16 464 64 464H384C392.8 464 400 456.8 400 448V192H48V448z"/></svg>
-                <p class="inline-flex ml-4 text-gray-400 mt-1">Due in 10 days</p>
-=======
                 <div class="w-56 h-2.5 ml-8 mt-1 dark:bg-gray-700">
                     PIC : {{ $data->sender_name }}
                 </div>
@@ -76,7 +69,6 @@
                         ?> <p class="inline-flex ml-4 mt-1"><?php echo "$days days $hours hours and $minutes minutes left" ?></p> <?php
                     }
                 ?>
->>>>>>> 8738a30adee1217647cb8e6087f2dd197d516d00
             </div>
         </div>
         <div class="mx-auto items-center">
