a80539ec2ecc1a999b845a59eeeb71ded6a866e5
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-10-15 16:35:58 +0700

    revisi part3

--- resources/views/modal/deleteTaskPIC.blade.php
+++ resources/views/modal/deleteTaskPIC.blade.php
@@ -12,10 +12,9 @@
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div class="p-6 text-center">
                 <svg aria-hidden="true" class="mx-auto mb-4 w-52 text-gray-400 dark:text-gray-200 stroke-red-600" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah anda yakin ingin menonaktifkan 
-                  data yang dipilih?</h3>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah anda yakin ingin menghapus data yang dipilih?</h3>
                   <div class="flex justify-center">
-                    <button data-modal-toggle="deleteTaskPIC" type="button" class="text-gray-50 mr-8 bg-gray-400 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-3xl border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                    <button data-modal-toggle="deleteTaskPIC" type="button" class="text-gray-50 mr-8 bg-gray-400 hover:bg-gray-100 rounded-3xl border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
                         No, cancel
                     </button>
                     <form action="" method="post">
