0d543770b08d3162c617248d1a4e0b74424cf0fb
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-10-15 20:02:40 +0700

    revisi part4

--- resources/views/modal/mytask.blade.php
+++ resources/views/modal/mytask.blade.php
@@ -112,7 +112,6 @@
                 </div>
               <!-- Modal footer -->
               <div class="flex items-center p-6 space-x-2 justify-center rounded-b border-t border-gray-200 dark:border-gray-600">
-                <button type="reset" class="text-gray-500 bg-white hover:bg-gray-100 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancel</button>
                   <button type="submit" class="text-white bg-mainclr hover:bg-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700">Submit</button>
               </div>
             </form>
--- resources/views/modal/taskcompleted.blade.php
+++ resources/views/modal/taskcompleted.blade.php
@@ -14,7 +14,113 @@
                 </h3>
             </div>
             <!-- Modal body -->
-            <div class="p-6">
+            <div class="overflow-x-auto relative pl-3">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <tbody class="">
+                        <tr class="bg-white dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="py-4 px-6 w-1/4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                No Tugas
+                            </th>
+                            <td class="py-4 px-6 w-1">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp;{{ $data->t_id }}
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Sumber Tugas
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                PIC - {{ $data->sender_name }}
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Nama Penerima
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                Ikhsan Nurul Rizki, Primarazaq
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Deadline
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp;{{ $date = date('l, d F Y, H.i A',strtotime($data->t_due_date)) }}
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Terakhir Diubah
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp;{{ $date = date('l, d F Y, H.i A',strtotime($data->updated_at)) }}
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Deskripsi Tugas
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp;<?php echo ($data->t_body) ?>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Your Response
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp;<?php echo ($data->response_body) ?>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Your Submission
+                            </th>
+                            <td class="py-4 px-6">
+                                :
+                            </td>
+                            <td class="py-4 px-6">
+                                &nbsp; <a href="{{ asset('storage/' . $data->response_file) }}" class="text-mainclr dark:text-teal-500 hover:underline">lihat file</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="py-4">
+                    <img src="../../imgs/done.png" class="w-16 mx-auto" alt="">
+                    <h3 class="text-2xl mx-auto font-bold text-gray-900 dark:text-white">
+                    Your Task is Complete
+                    </h3>
+                </div>
+            {{--<div class="py-4">
+                    <img src="../../imgs/undone.png" class="w-16 mx-auto" alt="">
+                    <h3 class="text-2xl mx-auto font-bold text-gray-900 dark:text-white">
+                        Tasks are not completed on time
+                    </h3>
+                </div> --}}
+            </div>
+            {{-- <div class="p-6">
                 <div class="flex">
                     <div class="basis-3/4 text-base space-y-5 text-left leading-relaxed text-gray-500 dark:text-gray-400">
                         <p>No. Tugas</p>
@@ -37,11 +143,8 @@
                         <p>:&nbsp; <a href="{{ asset('storage/' . $data->response_file) }}" class="text-mainclr dark:text-teal-500 hover:underline">lihat file</a></p>
                     </div>
                 </div>
-                <img src="../../imgs/done.png" class="w-16 mx-auto" alt="">
-                <h3 class="text-2xl mx-auto font-bold text-gray-900 dark:text-white">
-                    Your Task is Complete
-                </h3>
-            </div>
+                
+            </div> --}}
             <!-- Modal footer -->
             <div class="flex items-center pt-1 pb-1 space-x-2 rounded-b border-gray-200 dark:border-gray-600">
                 <button data-modal-toggle="modalTaskComplt-{{ $data->t_id }}" type="button" class=" mx-auto text-black font-bold bg-white hover:bg-gray-100 rounded-full border border-black text-sm px-14 py-2 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Back</button>
