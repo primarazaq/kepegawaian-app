49a48be49387bb96801368be834125a231d9b0e5
tyohd <andr9422@gmail.com>
2022-11-02 19:02:02 +0700

    add children active

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -171,7 +171,7 @@
    </a>
 </li>
 <li> 
-  <a href="/employee/home/mytask" class="{{ Request::is('employee/home/mytask') ? 'active' : '' }} flex items-center p-2 py-1 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-black hover:opacity-40 dark:hover:bg-black">
+  <a href="/employee/home/mytask" class="{{ Request::is('employee/home/mytask*') ? 'active' : '' }} flex items-center p-2 py-1 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-black hover:opacity-40 dark:hover:bg-black">
    <svg version="1.1" class="w-5 fill-white" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
    width="31.273px" height="31.273px" viewBox="0 0 31.273 31.273" style="enable-background:new 0 0 31.273 31.273;"
    xml:space="preserve">
@@ -188,7 +188,7 @@
   </a>
 </li>
 <li>
-   <a href="/employee/home/taskcompleted" class="{{ Request::is('employee/home/taskcompleted') ? 'active' : '' }} flex items-center p-2 py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-black hover:opacity-40 dark:hover:bg-black">
+   <a href="/employee/home/taskcompleted" class="{{ Request::is('employee/home/taskcompleted*') ? 'active' : '' }} flex items-center p-2 py-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-black hover:opacity-40 dark:hover:bg-black">
       <svg version="1.1" class="w-6 fill-white" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 491.5 491.5" style="enable-background:new 0 0 491.5 491.5;" xml:space="preserve">
 <g>
