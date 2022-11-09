1fe6e158e2a8331d73e4b6bfb79a3793cfe56a8a
primarazaq <85297003+primarazaq@users.noreply.github.com>
2022-11-10 04:03:19 +0700

    fix bug last backend only

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,33 +16,7 @@ class LoginController extends Controller
     }
 
     public function postlogin (Request $request){
-                    $deadline = DB::table('tasks')->select('id','t_due_date')->orderBy('t_due_date', 'asc')->get(); 
-                    
-                    //fungsi untuk membatasi deadline. Jika sudah melebihi due_date, maka status otomatis menjadi uncompleted
-
-                    foreach ($deadline as $item) {
-
-                        $seconds = strtotime($item->t_due_date) - time();
-
-                        $days = floor($seconds / 86400);
-                        $seconds %= 86400;
-
-                        $hours = floor($seconds / 3600);
-                        $seconds %= 3600;
-
-                        $minutes = floor($seconds / 60);
-                        $seconds %= 60;
-
-                        $task_id = $item->id;
-
-                        // dd($days);
-                        if ($days <= 0 || $hours < 0 || $minutes < 0) {
-                            Task::where('id', $task_id)->where('t_status', 'in progress')
-                                ->update(['t_status' => 'overdue']);
-                        }
-                    }
-
-
+                   
         if (Auth::attempt($request->only('nip','password','level'))){
 
             switch(auth()->user()->level){
